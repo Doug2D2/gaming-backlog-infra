be509b3957c59bce0c8e493b7a470c60e968af66
Matt Vella <mattvella07@gmail.com>
2022-05-22 14:48:19 -0500

    Format and update App Name tag

--- main.tf
+++ main.tf
@@ -1,39 +1,39 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 module "UsersTable" {
-    source = "./modules/db"
+  source = "./modules/db"
 
-    tableName  = "Users"
-    hashKey    = "UserID"
-    rangeKey   = ""
-    attributes = [{"name": "UserID", "type": "S"}]
+  tableName  = "Users"
+  hashKey    = "UserID"
+  rangeKey   = ""
+  attributes = [{ "name" : "UserID", "type" : "S" }]
 }
 
 module "BacklogTable" {
-    source = "./modules/db"
+  source = "./modules/db"
 
-    tableName  = "Backlog"
-    hashKey    = "UserID"
-    rangeKey   = "SK"
-    attributes = [{"name": "UserID", "type": "S"}, {"name": "SK", "type": "S"}]
+  tableName  = "Backlog"
+  hashKey    = "UserID"
+  rangeKey   = "SK"
+  attributes = [{ "name" : "UserID", "type" : "S" }, { "name" : "SK", "type" : "S" }]
 }
 
 module "WishlistTable" {
-    source = "./modules/db"
+  source = "./modules/db"
 
-    tableName  = "Wishlist"
-    hashKey    = "UserID"
-    rangeKey   = "SK"
-    attributes = [{"name": "UserID", "type": "S"}, {"name": "SK", "type": "S"}]
+  tableName  = "Wishlist"
+  hashKey    = "UserID"
+  rangeKey   = "SK"
+  attributes = [{ "name" : "UserID", "type" : "S" }, { "name" : "SK", "type" : "S" }]
 }
 
 module "CompletedTable" {
-    source = "./modules/db"
+  source = "./modules/db"
 
-    tableName  = "Completed"
-    hashKey    = "UserID"
-    rangeKey   = "SK"
-    attributes = [{"name": "UserID", "type": "S"}, {"name": "SK", "type": "S"}]
+  tableName  = "Completed"
+  hashKey    = "UserID"
+  rangeKey   = "SK"
+  attributes = [{ "name" : "UserID", "type" : "S" }, { "name" : "SK", "type" : "S" }]
 }
--- modules/db/main.tf
+++ modules/db/main.tf
@@ -1,20 +1,20 @@
 resource "aws_dynamodb_table" "table" {
-    name           = var.tableName
-    hash_key       = var.hashKey
-    range_key      = var.rangeKey
-    write_capacity = 5
-    read_capacity  = 5
+  name           = var.tableName
+  hash_key       = var.hashKey
+  range_key      = var.rangeKey
+  write_capacity = 5
+  read_capacity  = 5
 
-    dynamic "attribute" {
-        for_each = var.attributes
-        content {
-            name = attribute.value["name"]
-            type = attribute.value["type"]
-        }
+  dynamic "attribute" {
+    for_each = var.attributes
+    content {
+      name = attribute.value["name"]
+      type = attribute.value["type"]
     }
+  }
 
-    tags = {
-        Name = var.tableName
-        App  = "GameBacklog"
-    }
+  tags = {
+    Name = var.tableName
+    App  = "GamingBacklog"
+  }
 }
